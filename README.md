0d5ab2cce83cbc45d775bbca00697d9e790df802
Aidan O'Brien <aidan.obrien@saberastro.com>
2018-07-30 17:28:12 +1000

    Pre-Migration commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# pyScene
-Small library for representing satellite constellations and ground stations in python
+# Satellite-Constellation-Creator
+Library for representing satellite constellations and ground stations
+
++ Free software: MIT
+
+## Installation
+To install latest released version::
+    pip install satellite-constellation
+    
--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
 
 setup(name="SatelliteConstellationCreator",
-      version=0.1,
+      version="0.1",
       description="Create Satellite Constellations around the Earth, Luna and planets in the solar system",
       author="Aidan O'Brien - Saber Astronautics Australia",
       author_email="aidan.obrien@saberastro.com",
       url="https://github.com/SaberAidan/pyScene",
       packages=["satellite-constellation"],
-      )
+      package_dir={'scenecreator': 'scenecreator'},
+      keywords="satellite satellites constellation orbital mechanics",
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Licence :: MIT',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      test_suite='tests')
