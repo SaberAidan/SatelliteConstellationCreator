60179af1f645d7746e561f7926fc1a6d1543b439
Joseph Richmond <josef.richmond@saberastro.com>
2020-11-25 13:47:48 +1100

    Implemented streets of coverage type constellation in SOCConstellation class

--- satellite_constellation/Constellation.py
+++ satellite_constellation/Constellation.py
@@ -2,7 +2,7 @@
 Class for holding Constellations of Satellites within it.
 """
 from .Satellite import Satellite
-from .utils import heavenly_body_radius
+from .utils import heavenly_body_radius, heavenly_body_mass, proper_round
 import warnings
 import math
 
@@ -11,7 +11,6 @@ class Constellation(object):
     """
     Class for describing and holding a constellation of satellites
     """
-
     def __init__(self, num_sats, num_planes, phasing, inclination, altitude,
                  eccentricity, beam_width, name="Sat", focus="earth", starting_number=0):
         self.num_sats = num_sats
@@ -29,6 +28,7 @@ class Constellation(object):
         self.raan = self.__calculate_raan()
         self.ta = self.__calculate_ta()
         self.satellites = self.__build_satellites()
+        print(self.__str__())
 
     def __corrected_planes(self):
         sats_per_plane = int(self.num_sats / self.num_planes)
@@ -59,8 +59,9 @@ class Constellation(object):
         for i in range(self.num_sats):
             sat_num = i + self.start_num + 1
             sat_name = self.constellation_name + " " + str(sat_num)
+            # print('raan', self.raan[i] , ", perigee", self.perigee_positions[i] , ", ta" , self.ta[i])
             satellites.append(Satellite(sat_name, self.altitude, self.e, self.inclination, self.raan[i],
-                                        self.perigee_positions[i], self.ta[i], self.beam, focus=self.focus))
+                                        self.perigee_positions[i], self.ta[i], self.beam, focus=self.focus,rads = False))
         return satellites
 
     def __repr__(self):
@@ -98,59 +99,116 @@ class Constellation(object):
         return short_scene
 
 class SOCConstellation:
-    def __init__(self, street_width ,altitude, beam_width, raan, name="Sat", focus="earth", starting_number=0): #Start off with just a single polar orbit
-        self.inclination = 90 #Polar Orbit
+    def __init__(self, num_streets, street_width ,altitude, beam_width, raan, eccentricity, revisit_time, name="Sat", focus="earth", starting_number=0): #Start off with just a single polar orbit
+        self.inclination = 90  # Polar Orbit
+        self.num_streets = num_streets
         self.altitude = altitude
         self.beam = beam_width
         self.start_num = starting_number
         self.raan = raan
+        self.eccentricity = eccentricity
         self.constellation_name = name
         self.focus = focus
         self.street_width = street_width
+        self.revisit_time = revisit_time
         self.earth_coverage_radius, self.earth_coverage_angle = self.__calculate_earth_coverage()
         self.linear_spacing, self.angular_spacing = self.__calculate_spacing()
-        self.num_satellites = self.__calculate_required_satellites()
+        self.perigee, self.semi_major, self.orbital_period = self.__calculate_orbit_params()
+        self.num_sats, self.sats_per_street = self.__calculate_required_satellites()
+        self.perigee = self.__perigee_positions()
         self.ta = self.__calculate_ta()
+        print(self.num_sats, "satellites w/", self.angular_spacing , "degrees spacing")
+        self.satellites = self.__build_satellites()
+        print(self.__str__())
+
         # self.sats_per_plane, self.correct_phasing = self.__corrected_planes()
         # self.perigee_positions = self.__perigee_positions()
-        # self.ta = self.__calculate_ta()
         # self.satellites = self.__build_satellites()
-
-    def __calculate_spacing(self):
-        spacing = 360/self.num_sats
-        return spacing
-
-    def __calculate_ta(self):
-        ta = [0] * self.num_sats
-        # for i in range(self.sats_per_plane, self.num_sats):
-        #     ta[i] = ta[i - self.sats_per_plane] + self.correct_phasing
-        return ta
 
     def __calculate_earth_coverage(self):
         x = self.altitude * math.tan((math.pi/180)*self.beam/2)
         theta = math.asin(x/heavenly_body_radius[self.focus])
         r = heavenly_body_radius[self.focus]*theta
-        # print(r,theta*180/math.pi)
-
         return r, theta
 
     def __calculate_spacing(self):
         y = math.sqrt(math.pow(self.earth_coverage_radius,2)-math.pow(self.street_width/2,2))
         ang_spacing = 2*y/heavenly_body_radius[self.focus]
-        print(ang_spacing)
         return y, ang_spacing
 
+    def __calculate_orbit_params(self):
+        perigee = heavenly_body_radius[self.focus] + self.altitude  # [km]
+        semi_major = perigee/(1-self.eccentricity)  # [km]
+        orbital_period = 2*math.pi*math.sqrt((semi_major*10**3)**3/(heavenly_body_mass[self.focus]*6.67408*10**(-11)))  # [s]
+        return perigee, semi_major, orbital_period
+
     def __calculate_required_satellites(self):
-        num_satellites = round(math.pi*2/self.angular_spacing)
-        return num_satellites
+        num_satellites_a = self.orbital_period / self.revisit_time  # Calculated from revisit time
+        num_satellites_b = math.pi * 2 / self.angular_spacing  # Total coverage
+
+        if num_satellites_a > num_satellites_b:
+            num_satellites = num_satellites_b
+        else:
+            num_satellites = num_satellites_a
+
+        upper = math.ceil(num_satellites)
+        lower = math.floor(num_satellites)
+
+        if num_satellites - lower <= 0.01:
+            self.angular_spacing = 2*math.pi/lower
+            # print(self.angular_spacing)
+            self.angular_spacing = 360/lower
+            total_satellites = lower*self.num_streets
+            return total_satellites, lower
+        else:
+            # self.angular_spacing = 2*math.pi/upper
+            # print(self.angular_spacing)
+            self.angular_spacing = 360/upper
+            total_satellites = upper*self.num_streets
+            return total_satellites, upper
+
+    # def __calculate_ta(self):
+    #     ta = [0] * self.num_sats
+    #     for idx, anom in enumerate(ta):
+    #         ta[idx] = idx*self.angular_spacing
+    #     return ta
 
     def __calculate_ta(self):
-        ta = [0] * self.num_satellites
-        for idx, anom in enumerate(ta):
-            ta[idx] = idx*self.angular_spacing
-            print(idx,ta[idx])
+        phase = 360/self.num_sats
+        print(phase)
+        ta = [0] * self.num_sats
+        for i in range(self.num_streets):
+            for j in range(self.sats_per_street):
+                ta[i*self.sats_per_street + j] = i*phase
         return ta
 
+    def __perigee_positions(self):
+        perigees = list(range(0, 360, int(360/self.sats_per_street)))
+        all_perigees = []
+        for i in range(self.num_sats):
+            all_perigees.extend(perigees)
+        return all_perigees
 
 
+    def __build_satellites(self):
+        satellites = []
+        for i in range(self.num_streets):
+            for j in range(self.sats_per_street):
+                sat_num = i * self.sats_per_street + j + self.start_num + 1
+                sat_name = self.constellation_name + " " + str(sat_num)
+                satellites.append(Satellite(sat_name, self.altitude, self.eccentricity, self.inclination, self.raan[i],
+                                            self.perigee[j], self.ta[i*self.sats_per_street+j], self.beam, focus=self.focus,rads = False))
+        return satellites
 
+    def __repr__(self):
+        return "{0}, {1}, {2}, {3}, {4}".format(self.num_sats,
+                                                self.altitude, self.eccentricity,
+                                                self.beam,
+                                                self.constellation_name)
+
+    def __str__(self):
+        sat_string = ""
+        for sat in self.satellites:
+            sat_string += sat.__str__() + '\n'
+
+        return sat_string.rstrip()
