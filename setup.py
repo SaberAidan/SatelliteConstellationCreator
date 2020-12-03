65ecc1ac6812f03f081ed5de8c788710f134be5f
Aidan O'Brien <aidan.obrien@saberastro.com>
2020-12-03 17:49:38 +1100

    Updated to allow testing for Python 3.9

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name="satellite-constellation",
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Scientific/Engineering :: Astronomy',
           'Topic :: Scientific/Engineering :: Physics'
       ])
