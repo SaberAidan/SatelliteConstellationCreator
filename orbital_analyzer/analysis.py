Fixed some bugs in coordinate calculation

orbital_analyzer/analysis.py
@@ -418,7 +418,7 @@ class orbital_analysis:
 
         half_width = (satellites[:, 7] / 2) * math.pi / 180
 
-        half_width[half_width > np.arcsin((true_alt - alt) / true_alt)] = np.arcsin((true_alt - alt) / true_alt)
+        half_width[half_width > np.arcsin((true_alt - alt) / true_alt)] = np.arcsin((true_alt - alt) / true_alt)[half_width > np.arcsin((true_alt - alt) / true_alt)]
 
         theta_2 = np.arcsin((np.sin(half_width) / ((true_alt - alt) / true_alt))) - half_width
         r_2 = (true_alt - alt) * theta_2
@@ -438,8 +438,6 @@ class orbital_analysis:
 
         d_prop = 1
         overflow_ctr = 0
-        # step_res = 1
-        # overflow = 360
 
         num_sats = []
 
@@ -481,7 +479,7 @@ class orbital_analysis:
           :param custom_satellites: List of satellites to use in coverage calculation
           :param resolution:
 
-          """
+        """
 
         area = 0
         coords = self.constellation.as_geographic()

plotter/visualiser.py
@@ -68,7 +68,7 @@ def draw_walker_plotly(walker_constellation, satellites=True, orbits=True, links
             half_width = (walker_constellation.beam_width/2 * math.pi/180)
             if half_width > np.arcsin(rE / r):
                 half_width = np.arcsin(rE / r)
-            theta_2 = np.arcsin(np.sin(half_width) / ((rE) / (r))) - half_width
+            theta_2 = np.arcsin(np.sin(half_width) / (rE / r)) - half_width
 
             walker_constellation.earth_coverage_angle = theta_2
 

satellite_constellation/utils.py
@@ -314,7 +314,6 @@ def sat_to_xyz(satellite):
     Ohm = satellite.right_ascension_r
     E = math.sqrt(1 - e ** 2) * math.sin(satellite.ta_r) / (1 + e * math.cos(satellite.ta_r))
 
-
     # print('r', r, 'e', e, 'a', a, 'i', i, 'w', w, 'ohm', Ohm, 'E', E)
 
     x = a * ((math.cos(E) - e) * (math.cos(w) * math.cos(Ohm) - math.sin(w) * math.sin(Ohm) * math.cos(i)) + math.pow(
@@ -333,6 +332,16 @@ def sat_to_xyz(satellite):
 
 def sat_to_xyz_np(satellites):
 
+    """
+
+    Determines the cartesian coordinates of an array of satellites via their orbital elements \n
+    Method from https://ntrs.nasa.gov/citations/19650015945
+
+    :param satellites: The array of satellites to have coordinates determined
+
+    """
+
+
     r = satellites[:, 1]
     e = satellites[:, 2]
     a = satellites[:, 8]
@@ -344,7 +353,6 @@ def sat_to_xyz_np(satellites):
 
     E = np.sqrt(1 - e ** 2) * np.sin(ta) / (1 + e * np.cos(ta))
 
-    # print('r', r, 'e', e, 'a', a, 'i', i, 'w', w, 'ohm', Ohm, 'E', E)
 
     x = a * ((np.cos(E) - e) * (np.cos(w) * np.cos(Ohm) - np.sin(w) * np.sin(Ohm) * np.cos(i)) + np.power(
         1 - np.power(e, 2), 0.5) * np.sin(E) * (