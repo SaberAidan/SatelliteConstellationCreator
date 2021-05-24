bf95b2f9fe77ee38276ae76015b3741f042d4bb3
Sam Sipe <sam@sipe.io>
2021-05-24 14:36:14 -0400

    window names, stop plt.savefig in function

--- satellite_constellation/main.py
+++ satellite_constellation/main.py
@@ -1,8 +1,10 @@
 from satellite_constellation.visualiser import *
+import matplotlib.pyplot as plt
 
 def main():
     draw_walker(WalkerConstellation(8, 2, 1, 45, 1000, 0, 20))
     draw_flower(FlowerConstellation(37, 18, 57, 6, 19, 0, 0, 19702, 30))
+    plt.show()
 
 if __name__ == '__main__':
     main()
