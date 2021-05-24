2093792418f7d5b7da7b2f441c9edc4fa17b5041
Sam Sipe <sam@sipe.io>
2021-05-24 14:10:15 -0400

    wrap main.py function so it is callable

--- satellite_constellation/main.py
+++ satellite_constellation/main.py
@@ -1,5 +1,8 @@
 from satellite_constellation.visualiser import *
 
-if __name__ == '__main__':
+def main():
     draw_walker(WalkerConstellation(8, 2, 1, 45, 1000, 0, 20))
     draw_flower(FlowerConstellation(37, 18, 57, 6, 19, 0, 0, 19702, 30))
+
+if __name__ == '__main__':
+    main()
